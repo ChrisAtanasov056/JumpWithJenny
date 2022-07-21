fe2301d01d6834c24e9813df7556c02f90e4b6f8
ChrisAtanasov056 <62712886+ChrisAtanasov056@users.noreply.github.com>
2022-07-21 18:10:18 +0300

    Adding ScheduleController

---
+++ ServerAPI/ServerAPI/Controllers/ScheduleController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.Json;
+using ServerAPI.Models.Schedule;
+using ServerAPI.Services.Schedule;
+using System.Text.Json;
+
+namespace ServerAPI.Controllers
+{
+    public class ScheduleController : Controller
+    {
+        private readonly IScheduleService scheduleService;
+
+        public ScheduleController(IScheduleService scheduleService)
+        {
+            this.scheduleService = scheduleService;
+        }
+
+        // GET: ScheduleController
+        [Route("Schedule/Index")]
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var viewModel = await this.scheduleService.GetAllWorkoutsAsync<WorkoutViewModels>();
+            string json = JsonSerializer.Serialize(viewModel);
+            return Json(json);
+        }
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //// GET: ScheduleController/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //// POST: ScheduleController/Create
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create(IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: ScheduleController/Edit/5
+        //public ActionResult Edit(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: ScheduleController/Edit/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: ScheduleController/Delete/5
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: ScheduleController/Delete/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Delete(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+    }
+}
---
+++ ServerAPI/ServerAPI/Services/Schedule/IScheduleService.cs
@@ -0,0 +1,15 @@
+﻿namespace ServerAPI.Services.Schedule
+{
+    public interface IScheduleService
+    {
+        Task<List<T>> GetAllWorkoutsAsync<T>();
+        Task<T> GetByIdAsync<T>(string id);
+       
+        //TO DO: 
+
+        //Task DeleteAsync(string id);
+        //Task<Workout> AddUserToEvent(string userId, string eventId);
+        //Task AddAsync(int classId, DateTime startEvent, DateTime endEvent, int availableSpots, string description);
+
+    }
+}
