8a54f13a44a13b4b0745c37ca59f5f805cd8d82f
ChrisAtanasov056 <62712886+ChrisAtanasov056@users.noreply.github.com>
2024-12-19 14:23:39 +0200

    api/front end

--- ServerAPI/ServerAPI/Controllers/UserController.cs
+++ ServerAPI/ServerAPI/Controllers/UserController.cs
@@ -6,8 +6,6 @@ using ServerAPI.Models;
 using ServerAPI.Services.Users;
 using ServerAPI.ViewModels.Users;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace ServerAPI.Controllers
 {
 
