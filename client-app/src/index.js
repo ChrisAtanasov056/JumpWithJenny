d74253c56e723974dae8775c73e094397f137307
ChrisAtanasov056 <chris.atanasov056@gmail.com>
2025-07-23 13:50:11 +0300

    Add Bootstrap and Slick Carousel CSS; remove unused custom JS and Flaticon files

--- client-app/src/index.js
+++ client-app/src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// Импортирай bootstrap css от node_modules
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Импортирай slick carousel css, ако го ползваш
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+// Твоят собствен css
 import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
-      <App />
-  
+  <App />
 );
+
 reportWebVitals();
